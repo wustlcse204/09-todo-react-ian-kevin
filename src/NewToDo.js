39c6e21e6325a519ac004804c1c7c4fb3d2c5661
iandmacdonald <59847619+iandmacdonald@users.noreply.github.com>
2022-11-28 18:34:45 -0600

    j

---
+++ src/NewToDo.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './NewToDo.css';
+
+class NewToDo extends Component {
+  render() {
+    return(
+        <div>
+        <form>
+            <label id="todoForm">Input To-Do:</label>
+            <input id = "txtbox" type="text" name="todoForm"/>
+            <input id = "enter_button" type="submit" value="ENTER" onclick="post()"/>
+
+        </form>
+        </div>
+    );
+
+  }
+}
+
+export default NewToDo;
